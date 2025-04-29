01eb7d111e33b85d5fee2a9af9f2733d1eb589ff
EeroKoivukoski <eero.koivukoski@metropolia.fi>
2025-04-29 15:41:22 +0300

    Delete test.py

--- PPM_Heart_mod/test.py
+++
@@ -1,9 +0,0 @@
-from led     import Led
-from time    import sleep
-from machine import Pin, ADC, I2C
-from fifo    import Fifo
-
-adc = ADC(26)
-while True:    
-    print(adc.read_u16())
-    sleep(0.05)
