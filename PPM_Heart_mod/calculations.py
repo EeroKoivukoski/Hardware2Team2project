45e30eaac07cbb35d6c9d08b45a6b7ddc07b70b2
HarseDz <harisdzino2004@gmail.com>
2025-04-30 15:57:44 +0300

    Update calculations.py
    sdnn, cleanup

--- PPM_Heart_mod/calculations.py
+++ PPM_Heart_mod/calculations.py
@@ -1,12 +1,13 @@
 import math
 import test
 
+#call all of the functions and return them in a list
 def calculate(ppis):
     meanppi=calc_meanppi(ppis)
     meanhrv=calc_meanhrv(ppis,meanppi)
     rmssd=calc_rmssd(ppis)
-    sdnn="wip"
-    sns="wip"
+    sdnn=calc_sdnn(ppis)
+    sns="wip" #sns & pns on kubios
     pns="wip"
     return [meanhrv,
             meanppi,
@@ -14,29 +15,36 @@ def calculate(ppis):
             sdnn,
             sns,
             pns]
+
 #calculate mean heart rate value
 def calc_meanhrv(ppis,meanppi):
-    meanhrv = 60/(meanppi/1000)
-    return meanhrv
+    return 60/(meanppi/1000)
+    
 
 #calculate mean ppi
 def calc_meanppi(ppis):
     sum = 0
     for i in ppis:
         sum += i
-    meanppi = sum/len(ppis)
-    return meanppi
+    return sum/len(ppis)
+    
 
 #calculate rmssd
 def calc_rmssd(ppis):
-    diff = listdiff(ppis)
-    squarediff = [j**2 for j in diff]
+    meandiff = calc_ppidiff(ppis)
+    squarediff = [j**2 for j in meandiff]
     meansquared = sum(squarediff) / len(squarediff)
-    rmssd = math.sqrt(meansquared)
-    return rmssd
+    return math.sqrt(meansquared)
+    
 
-def listdiff(ppis):
+#calculate the differences in peak to peak intervals (ppi) and compile them into a list.
+def calc_ppidiff(ppis):
     return [ppis[i+1] - ppis[i] for i in range(len(ppis)-1)]
 
-
-#long ahh rmssd calculation ting function (difference between peaks squared then the average of that square-rooted or something)
+#calculate sdnn
+def calc_sdnn(ppis):
+    meandiffs = calc_ppidiff(ppis)
+    for z in ppis:
+        deviation += (z - meandiffs)**2
+    return sqrt(deviation/(len(ppis)-1))
+    
