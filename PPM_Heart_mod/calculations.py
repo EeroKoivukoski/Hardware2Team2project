773161072d78b9faa3d686acc608eae2bacc6611
HarseDz <harisdzino2004@gmail.com>
2025-04-30 12:32:18 +0300

    Update calculations.py

--- PPM_Heart_mod/calculations.py
+++ PPM_Heart_mod/calculations.py
@@ -10,27 +10,38 @@ import test
     
 beatz = 47
 time = 7500
-ppis=[156,154,155,160, 170, 150, 181, 150, 167, 177,156,154,155,160, 170, 150, 181, 150, 167, 177,156,154,155,160, 170, 150, 181, 150, 167, 177,156,154,155,160, 170, 150, 181, 150, 167, 177]
-#ppis=test.calculate_hr()
+#ppis=[156,154,155,160, 170, 150, 181, 150, 167, 177,156,154,155,160, 170, 150, 181, 150, 167, 177,156,154,155,160, 170, 150, 181, 150, 167, 177,156,154,155,160, 170, 150, 181, 150, 167, 177]
+ppis=test.calculate_hr()
 count = 0
-sumppis = 0
 
-# "meanhr" is the mean heart rate, take the beats calculated in 10 seconds (2500ms) and times that by 6 (=60 seconds, bpm)
 
-meanhr = len(ppis) * 2
-print(meanhr)
+# "meanhr" is the mean heart rate, take the beats calculated in 30 seconds (7500ms) and times that by 2 (=60 seconds, bpm)
 
-# calc ppis together and calc how many of them there are
-for i in ppis:
-    sumppis = sumppis + i
-#then if there are more than 0 of them calc the mean
-if len(ppis) > 0:
+#meanhr = len(ppis) * 2
+#print(meanhr)
+
+
+def meanhrppi(ppis):
+    sumppis = 0
+    for i in ppis:
+        sumppis += i
     meanppi = sumppis/len(ppis)
-    print(meanppi)
+    meanhr = 60/(meanppi/1000)
+    return meanhr, meanppi
+
+def listdiff(ppis):
+    return [ppis[i+1] - ppis[i] for i in range(len(ppis)-1)]
+# calc ppis together and calc how many of them there are
+#for i in ppis:
+    #sumppis = sumppis + i
+#then if there are more than 0 of them calc the mean
+#if len(ppis) > 0:
+    #meanppi = sumppis/len(ppis)
+    #print(meanppi)
 
 #long ahh rmssd calculation ting function (difference between peaks squared then the average of that square-rooted or something)
 def rmssdcalc(ppis):
-    diff = [ppis[i+1] - ppis[i] for i in range(len(ppis)-1)]
+    diff = listdiff(ppis)
     squarediff = [j**2 for j in diff]
     meansquared = sum(squarediff) / len(squarediff)
     rmssd = math.sqrt(meansquared)
@@ -38,4 +49,5 @@ def rmssdcalc(ppis):
 
 
 rmssdvalue = rmssdcalc(ppis)
-print(rmssdvalue) 
+meanhrppivalue = meanhrppi(ppis)
+print("rmssd = ",rmssdvalue, ", mean heart rate and PPI = ", meanhrppivalue) 
