436d349b4b7ffa7cf255ecaa1c44d89cd9a8582d
EeroKoivukoski <eero.koivukoski@metropolia.fi>
2025-04-30 15:10:27 +0300

    animated a heart to tell when device is calibrated

--- PPM_Heart_mod/test.py
+++ PPM_Heart_mod/test.py
@@ -1,15 +1,11 @@
 #imports
-import time
+import time,framebuf,smiley_bitmap,x_bitmap
 from ssd1306  import SSD1306_I2C as screen
 from machine  import Pin, ADC, I2C
-import framebuf,smiley_bitmap,x_bitmap
 lcd                = screen(128, 64, I2C(1, scl=Pin(15), sda=Pin(14)))
 adc                = ADC(26)
 
 def calculate_hr():
-    #bitmaps
-    imagey = framebuf.FrameBuffer(smiley_bitmap.img, 128, 64, framebuf.MONO_VLSB)
-    imagen = framebuf.FrameBuffer(x_bitmap.img, 128, 64, framebuf.MONO_VLSB)
     
     #settings
     accuracy       = 1 #The less accuracy the faster it calibrates, but the peak to peak diff suffers. (1=highest accuracy)
@@ -25,6 +21,10 @@ def calculate_hr():
     current_pos    = 0     #Current position on the lcd
     haspeaked      = False #If a peak happened, it will be true
     update         = False #If lcd is updated
+    calib          = False #If calibrated
+    hearts = framebuf.FrameBuffer(smiley_bitmap.img, 128, 64, framebuf.MONO_VLSB)
+    heartx = framebuf.FrameBuffer(x_bitmap.img, 128, 64, framebuf.MONO_VLSB)
+    
     
     #reset lcd just to be sure
     lcd.fill(0)
@@ -33,18 +33,6 @@ def calculate_hr():
     #True loop
     while True:
         
-        #calibration
-        if min_ <= 15000 or max_ >= 60000:
-            peaks.clear()
-            lcd.blit(imagen,0,0)
-        elif update == True:
-            lcd.blit(imagey,0,0)
-            
-        #lcd update
-        if update==True:
-            lcd.show()
-            update=False 
-            
         #Get sensor value
         current = adc.read_u16()
         
@@ -77,11 +65,30 @@ def calculate_hr():
             current_pos+=2
             if current_pos >= 128:
                 lcd.fill(0)
+                #animation
+                if calib:
+                    lcd.blit(hearts,0, 0)
+                else:
+                    lcd.blit(heartx,0,0)
                 current_pos = 0
             update=True  
             count=accuracy
         count-=1
-             
+        
+        #calibration
+        if min_ <= 15000 or max_ >= 60000:
+            peaks.clear()
+            lcd.rect(0,0,4,4,0)
+            calib=False
+        elif update == True:
+            lcd.rect(0,0,4,4,1)
+            calib=True
+           
+        #lcd update
+        if update==True:
+            lcd.show()
+            update=False
+            
         #End when you have 30 peaks
         if len(peaks)==31:
             #Make a list of peak to peak diff.
@@ -89,5 +96,4 @@ def calculate_hr():
             for i in range(1,31):
                 calpeaks.append(time.ticks_diff(peaks[i],peaks[i-1]))
             return calpeaks
-        
 print(calculate_hr())
